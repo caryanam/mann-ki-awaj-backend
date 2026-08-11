8db8ccea90fce84c3e29ccd5538c979d6f7d1691
caryanamPratikp <pratikpatil@caryanamindia.in>
2026-08-11 11:49:49 +0530

    8085 - 8089

--- src/main/java/com/mka/config/SocketIOConfig.java
+++ src/main/java/com/mka/config/SocketIOConfig.java
@@ -1,17 +1,24 @@
 package com.mka.config;
 
 import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SocketIOConfig {
 
+    @Value("${socket.io.port:8085}")
+    private int socketIoPort;
+
+    @Value("${socket.io.host:0.0.0.0}")
+    private String socketIoHost;
+
     @Bean
     public SocketIOServer socketIOServer() {
         com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
-        config.setHostname("0.0.0.0");
-        config.setPort(8085);
+        config.setHostname(socketIoHost);
+        config.setPort(socketIoPort);
         config.setOrigin("*");
         return new SocketIOServer(config);
     }
